7ed7d19deca78e6b8bd163aa6528d7f0585e19ae
Nattapol Krobklang (DHL TH), external <nattapol.krobklang@dhl.com>
2024-06-25 20:01:53 +0700

    first commit

---
+++ src/server/webapp.js
@@ -0,0 +1,22 @@
+const { render } = require('../functions/utils');
+
+const Route = {};
+Route.path = function (routeName, callback) {
+  Route[routeName] = callback;
+};
+
+function loadUi() {
+  return render('index');
+}
+
+const doGet = (e) => {
+  Route.path('project-list', loadUi);
+  if (Route[e.parameters.v]) {
+    return Route[e.parameters.v]();
+  }
+  return render('404');
+};
+
+module.exports = {
+  doGet,
+};
